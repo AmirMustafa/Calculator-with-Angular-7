25cc8199dc29e3d05b8d75b93970cae8d7c4e801
Amir Mustafa <amirengg15@gmail.com>
2019-04-14 14:16:39 +0530

    Updated title message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculator in Angular 6';
+  title = 'Calculator in Angular 7';
 }
